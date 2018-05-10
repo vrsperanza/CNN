90c8262e7e32975dc4d288cfd2f14d75e5ac9184
vrsperanza <vrsperanza@gmail.com>
2018-05-10 02:06:56 -0300

    Added new layers
    MaxPool, Ravel and ReLU

--- layers.py
+++ layers.py
@@ -1,9 +1,10 @@
 import numpy as np
+import math
 
 def squaredErrorBackpropagation(expectedResult, calculatedResult):
 	return -2*(expectedResult-calculatedResult)
 	
-class Sigmoid:
+class SigmoidLayer:
 	def forward(self, input):
 		self.output = 1/(1+np.exp(-input))
 		return self.output
@@ -11,7 +12,7 @@ class Sigmoid:
 	def backward(self, backpropagation, step):
 		return backpropagation*self.output*(1-self.output)
 
-class ReLU:
+class ReLULayer:
 	def forward(self, input):
 		self.output = np.maximum(np.zeros_like(input), input)
 		return self.output
@@ -20,7 +21,14 @@ class ReLU:
 		self.output[self.output > 0] = 1
 		return self.output
 	
+class RavelLayer:
+	def forward(self, input):
+		self.inputShape = input.shape
+		return input.ravel()
 		
+	def backward(self, backpropagation, step):
+		return np.reshape(backpropagation, self.inputShape)
+	
 class DenseLayer:
 	def __init__(self, inputSize, layerSize):
 		self.weights = np.random.normal(loc=0, scale=1, size=(inputSize+1, layerSize))
@@ -30,7 +38,7 @@ class DenseLayer:
 		self.output = np.matmul(np.append(input, [1], axis = 0), self.weights)
 		return self.output
 		
-	def backward(self, backpropagation, step):	
+	def backward(self, backpropagation, step):
 		inputAndTheta = np.append(self.input, [1], axis = 0)
 		self.weights -= step*np.matmul(inputAndTheta.reshape(-1, 1), backpropagation.reshape(1, -1))
 
@@ -44,22 +52,23 @@ class ConvolutionLayer:
 		self.kernels = []
 		for i in range(kernelAmount):
 			self.kernels.append(np.random.normal(loc=0, scale=1, size=(kernelShape[0], kernelShape[1])))
-			print(self.kernels[i])
 	
 	def forward(self, inputMatrix):
 		matrix = np.pad(inputMatrix, (int(self.kernelShape[0]/2), int(self.kernelShape[1]/2)), 'constant')
+		
 		matrixFFT = np.fft.fft2(matrix)
 		
 		self.input = matrix
-		self.output = []
+		self.output = np.empty((len(self.kernels), inputMatrix.shape[0], inputMatrix.shape[1]))
 		filter = np.zeros(matrix.shape)
-		for kernel in self.kernels:
+		for k in range(len(self.kernels)):
+			kernel = self.kernels[k]
 			filter[:kernel.shape[0],:kernel.shape[1]] = kernel
 			kernelFFT = np.fft.fft2(filter)
 
-			self.output.append( np.real(np.fft.ifft2(np.multiply(matrixFFT, kernelFFT)) \
-								[int(kernel.shape[0]/2):int((matrix.shape[0]-kernel.shape[0]/2)+1), 
-								int(kernel.shape[1]/2):int((matrix.shape[1]-kernel.shape[1]/2)+1)]))
+			self.output[k] = np.real(np.fft.ifft2(np.multiply(matrixFFT, kernelFFT)) \
+									[int(kernel.shape[0]/2):int((matrix.shape[0]-kernel.shape[0]/2)+1), 
+									int(kernel.shape[1]/2):int((matrix.shape[1]-kernel.shape[1]/2)+1)])
 		return self.output
 		
 	def backward(self, backpropagation, step):
@@ -69,12 +78,39 @@ class ConvolutionLayer:
 				for j in range(kernel.shape[1]):
 					kernel[i,j] -= step*np.mean(np.dot(self.input[i:i+backpropagation[k].shape[0],j:j+backpropagation[k].shape[1]], backpropagation[k]))
 		
-		# TO-DO: optionally propagate error
+		# TO-DO: Backpropagate error
 
 class MaxPoolLayer:
 	def __init__(self, poolShape, poolStride):
 		self.poolShape = poolShape
 		self.poolStride = poolStride
+		
+	def forward(self, inputMatrices):	
+		poolStride = self.poolStride
+		poolShape = self.poolShape
+	
+		self.inputMatrices = inputMatrices
+		outputMatrices = np.empty((inputMatrices.shape[0], int(inputMatrices.shape[1]/poolStride[0]), int(inputMatrices.shape[2]/poolStride[1])))
+		for k in range(outputMatrices.shape[0]):
+			for i in range(outputMatrices.shape[1]):
+				for j in range(outputMatrices.shape[2]):
+					outputMatrices[k,i,j] = np.max(inputMatrices[k, poolStride[0]*i:poolStride[0]*i+poolShape[0],poolStride[1]*j:poolStride[1]*j++poolShape[1]])
+		return outputMatrices
+		
+	def backward(self, backpropagation, step):
+		inputMatrices = self.inputMatrices
+		poolStride = self.poolStride
+		poolShape = self.poolShape
+		
+		backpropMatrices = np.zeros((inputMatrices.shape[0], inputMatrices.shape[1], inputMatrices.shape[2]))
+		for k in range(backpropMatrices.shape[0]):
+			for i in range(int(backpropMatrices.shape[1]/poolStride[0])):
+				for j in range(int(backpropMatrices.shape[2]/poolStride[1])):
+					slice = backpropMatrices[k, poolStride[0]*i:poolStride[0]*(i+1),poolStride[1]*j:poolStride[1]*(j+1)]
+					
+					maxInd =  np.unravel_index(np.argmax(slice), slice.shape)
+					backpropMatrices[k, poolStride[0]*i+maxInd[0],poolStride[1]*j+maxInd[1]] += 1
+		return backpropMatrices
 		
 class CNN:
 	def __init__(self):
--- main.py
+++ main.py
@@ -10,101 +10,51 @@ def oneHot(value, max):
 	arr[value] = 1
 	return arr
 
-def fourierConv(img, filter):
-	f = np.zeros(img.shape)	
-	f[:filter.shape[0],:filter.shape[1]] = filter
-	
-	FTImg = np.fft.fft2(img)
-	FTFilter = np.fft.fft2(f)
-	
-	return np.real(np.fft.ifft2(np.multiply(FTImg, FTFilter)))
-
-def MLPXorTest():
-	layer1 = DenseLayer(2, 5)
-	layer2 = DenseLayer(5, 1)
-
-	medianError = 0
-
-	for train in range(100000):
-		input = np.array((np.random.randint(0, 2), np.random.randint(0, 2)))
-		output = input[0] ^ input[1]
-		
-		ans = layer2.forward(layer1.forward(input))
-		
-		medianError = (99*medianError+np.power(output-ans, 2))/100
-		
-		err = squaredErrorBackpropagation(output, ans)
-		
-		step = 0.1
-		layer1.backpropagation(layer2.backpropagation(err, step), step)
-		
-		if train%1000 is 0:
-			print(train, input, output, ans, (ans > 0.5) == (output > 0.5), medianError)
-			
-def ConvolutionTest():
-	trainImages = mnist.trainingImages()
-	
-	kernelToApproach = np.matrix([[1/9,1/9,1/9],[1/9,1/9,1/9],[1/9,1/9,1/9]])
-	
-	step = 5e-5
-	
-	layer = ConvolutionLayer((3,3), 1)
-	
-	medianError = 1
-	for i in range(100000):
-		imgOrig = trainImages[random.randint(0, len(trainImages)-1)].astype(np.float32)/255;
-		imgTarget = fourierConv(imgOrig, kernelToApproach);
-		conv = layer.forward(imgOrig)
-		err = squaredErrorBackpropagation(imgTarget, conv[0])
-		layer.backpropagation(err, step)
-		
-		error = np.sqrt(np.mean(np.power(imgTarget-conv[0], 2)))
-		medianError = (99*medianError+error)/100
-		
-		step *= 0.99995
-		
-		if i % 1000 is 0:
-			print(layer.kernels[0])
-			print(i, error, medianError, step)
-	
-
-	plt.subplot(3, 1, 1)
-	plt.imshow(imgOrig, cmap='gray')
-
-	plt.subplot(3, 1, 2)
-	plt.imshow(imgTarget, cmap='gray')
-	
-	plt.subplot(3, 1, 3)
-	plt.imshow(conv[0], cmap='gray')
-	plt.show()
-
-def MLPMNISTTest(step = 0.01):
+def MNISTtrainCNN(cnn, step=0.000001, maxIterations=1000000, classes=3):
 	trainImages = mnist.trainingImages()
 	trainLabels = mnist.trainingLabels()
-	
-	cnn = CNN()
-	cnn.addLayer(DenseLayer(28*28, 20))
-	cnn.addLayer(Sigmoid())
-	cnn.addLayer(DenseLayer(20, 3))
-	cnn.addLayer(Sigmoid())
 	
 	medianError = 0
 
 	for train in range(1000000):
 		trainCase = random.randint(0, len(trainImages)-1)
-		
-		if(trainLabels[trainCase] >= 3):
-			train -= 1
+	
+		if(trainLabels[trainCase] >= classes):
 			continue
 		
-		input = trainImages[trainCase].ravel()
-		label = oneHot(trainLabels[trainCase], 3)
+		input = trainImages[trainCase]
+		label = oneHot(trainLabels[trainCase], classes)
 		
 		ans = cnn.train(input, label, step)
-		
+	
 		medianError = (99*medianError+abs(label-ans))/100
 		
-		if train%1000 is 0:
+		if train%100 is 0:
 			print(train, label, ans, medianError.mean())
-			
-MLPMNISTTest()
+	
+def MLPMNISTTest(step = 0.01):
+	cnn = CNN()
+	cnn.addLayer(RavelLayer())
+	cnn.addLayer(DenseLayer(28*28, 20))
+	cnn.addLayer(SigmoidLayer())
+	cnn.addLayer(DenseLayer(20, 3))
+	cnn.addLayer(SigmoidLayer())
+	
+	MNISTtrainCNN(cnn)
+
+def CNNMNISTTest(step = 0.01):
+	cnn = CNN()
+	
+	cnn.addLayer(ConvolutionLayer((3,3), 4))
+	cnn.addLayer(ReLULayer())
+	cnn.addLayer(MaxPoolLayer((2,2), (2,2)))
+	
+	cnn.addLayer(RavelLayer())
+	cnn.addLayer(DenseLayer(4*14*14, 10))
+	cnn.addLayer(SigmoidLayer())
+	cnn.addLayer(DenseLayer(10, 10))
+	cnn.addLayer(SigmoidLayer())
+	
+	MNISTtrainCNN(cnn, classes=10)
+
+CNNMNISTTest()
